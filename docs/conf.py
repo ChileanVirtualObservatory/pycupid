9fd25a25d3f9cb102b37ebd15ea7e8d9853d6796
mavillan <martin.villanueva@alumnos.usm.cl>
2017-05-22 15:52:38 -0400

    Removed numpydoc module for problems with readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
-    'numpydoc'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
