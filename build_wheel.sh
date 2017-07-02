2f47a41bf838bee6289222baafa09847bcec87d5
Camilo Valenzuela <cacke91@gmail.com>
2017-07-02 23:52:50 +0000

    Added twine in build wheel script

--- build_wheel.sh
+++ build_wheel.sh
@@ -2,6 +2,8 @@
 echo "Looking for py$1 environment"
 (source activate py$1 2>/dev/null && echo "Environment found" && python setup.py bdist_wheel --plat-name $2 && source deactivate)  || (echo "Environment py$1 not found, creating" && conda create -y --name py$1 python=$1 numpy cython && source activate py$1 && python setup.py bdist_wheel --plat-name $2 && source deactivate)
 
+pip install twine
+
 if [ "$2" = "TMP" ]; then
 	before="TMP.whl"
 	after="macosx_10_6_intel.macosx_10_9_intel.macosx_10_9_x86_64.macosx_10_10_intel.macosx_10_10_x86_64.whl"
